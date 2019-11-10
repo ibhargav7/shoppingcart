7b405e63262df17d9c3a6704fd54a3a80b8738cd
Bhargav Inturi <bhargavinturi792@gmail.com>
2019-11-10 16:19:29 +0530

    update cart.php

--- cart.php
+++ cart.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPEhtml>
 <html lang="en">
     <head>
@@ -8,78 +11,101 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     </head>
+    <script>
+    function Ajax(x) {
+  
+        $.post('cart.php',{id:x})
+
+      };
+    
+     </script>
 
     <style>
         #container{
-            background-color : #79d2a6
+            background-color: greenyellow;
         }
     </style>
 
     <body>
         <div class="jumbotron" id= "container">
         <div class="container text-center">
-            <h1>Your Cart</h1>    
+        <?php
+            $email=$_SESSION['email'];
+            echo "<a href='buy.php'><h3 style='text-align:right'>Back to Store</h3></a>"
+        ?>    
+        <h1>Your Cart</h1>    
         </div>
         </div>
         <div class="container">
             <table class="table table-bordered">
                 <thead>
                     <tr>
-                        <th>id</th>
+                        <th>Image</th>
                         <th>Name</th>
                         <th>Price(in ₹)</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
             
                         <?php
-                        $servername = "localhost";
-                        $username = "bhargav";
-                        $password = "iBhargav@1";
+                 
 
-                        $conn = new mysqli($servername,$username,$password,"bhargav");
+                        $conn = new mysqli("localhost", "bhargav", "iBhargav@1", "bhargav");
                         if ($conn->connect_error){
                             die("connection failed: ".$conn->connect_error);
                         };
-                        if (isset($_GET['id'])) {
-                            $id = mysqli_real_escape_string($conn,$_GET['id']);
+                        $email=$_SESSION['email'];
 
-                          
                         
-                            $sql = "SELECT * FROM shop WHERE id =".$id ;
+                        $sql3 = 'SELECT * FROM `cart` WHERE email="'.$email.'"';
                         
-                            $result = mysqli_query($conn,$sql);
-                            if (!$result){
-                                die("error");
-                            }
-                       
-                            
-                            $product=mysqli_fetch_assoc($result);
-                            $sql2 = "INSERT INTO `cart`(`id`, `name`, `price`) VALUES (".$product['id'].",\"".$product['name']."\",".$product['price'].")";
-                         
-                            $result2 = mysqli_query($conn,$sql2);
-                            $sql3 = 'SELECT * FROM cart';
-                            $result3 = mysqli_query($conn,$sql3);
-                            
-                            $cproducts = mysqli_fetch_all($result3,MYSQLI_ASSOC);
-                           
-                            foreach ($cproducts as $cproduct){
-                            
-                                echo "<tr><td>". $cproduct['id']."</td><td>". $cproduct['name']."</td><td>". $cproduct['price']."</td></tr>";
-                            }
-                            $sql4 = 'SELECT sum(price) as total FROM cart';
-                            $result4 = mysqli_query($conn,$sql4);
-                            $row=mysqli_fetch_array($result4);
-                            $total=$row["total"];
+                        $result3 = mysqli_query($conn,$sql3);
+
                         
-                            
-                            $conn->close();
+                        $num=0;
+                  
+
+                        while ($row = mysqli_fetch_assoc($result3)) {
+                          $num++;
                         }
+                        $result4= mysqli_query($conn,$sql3);
+                        $cproducts = mysqli_fetch_all($result4,MYSQLI_ASSOC);
+                        $url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+                        
+                        foreach ($cproducts as $cproduct){
+                        
+                            echo "<tr><td><img src='". $cproduct['image']."' style='width: 70px'</img></td><td>". $cproduct['name']."</td><td>". $cproduct['price']."</td><td><button class='btn btn-primary' onclick='Ajax(".$cproduct['id'].")'>Delete</button></td></tr>";
+                        }
+
+
+                        if (isset($_POST['id'])) {
+                            echo $_POST['id'];
+                            
+                            $conn = new mysqli("localhost", "bhargav", "iBhargav@1", "bhargav");
+                            if ($conn->connect_error){
+                                die("connection failed: ".$conn->connect_error);
+                            $id=$_GET['id'];
+                                $sql4='DELETE FROM `cart` WHERE id='.$id.'';
+                                echo "$sql4";
+                            };
+                        }
+ 
+                        $sql4 = 'SELECT sum(price) as total FROM cart WHERE email="'.$email.'"';
+                        $result4 = mysqli_query($conn,$sql4);
+                        $row=mysqli_fetch_array($result4);
+                        $total=$row["total"];
+                        $_SESSION["email"]=$email;
+
+                    
+                        
+                        $conn->close();
+                    
                         ?>
                     <tr>
                         <td><h4>Total Price<h4></td>
                         <td></td>
-                        <td><?php echo"$total" ?></td>
+                        <td><?php  echo "$total";?></td>
                     </tr>
 
                 </tbody>
